e470254ffa57b037cbc73dc1371af8eca6813006
DeyKast <deykast@users.noreply.github.com>
2023-06-02 15:07:27 +0300

    fix

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState, Component } from 'react';
+import React, { useState } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const Searchbar = ({ handleSubmit }) => {
